3cbef763ded850e3b768b51663b73767b33c09d2
Kencho5 <giokenchadze@gmail.com>
2024-08-10 17:26:51 +0400

    Updated: routes

--- src/register_routes.rs
+++ src/register_routes.rs
@@ -3,11 +3,11 @@ use crate::routes::*;
 
 pub fn register_routes(app: &mut Server<()>) {
     // AUTH ROUTES
-    app.at("/login")
+    app.at("/api/login")
         .with(GovernorMiddleware::per_hour(600).unwrap())
         .post(login::login_handler);
 
-    app.at("/register")
+    app.at("/api/register")
         .with(GovernorMiddleware::per_hour(600).unwrap())
         .post(register::register_handler);
 }
